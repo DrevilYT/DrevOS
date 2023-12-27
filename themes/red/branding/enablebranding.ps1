5b01765d9f1934013513b083341cd843b786c58c
DrevilYT <68735288+DrevilYT@users.noreply.github.com>
2023-12-27 20:32:37 +0800

    Update enablebranding.ps1

--- themes/red/branding/enablebranding.ps1
+++ themes/red/branding/enablebranding.ps1
@@ -1,6 +1,6 @@
 cd $env:USERPROFILE\AppData\Roaming\Microsoft\Windows\Themes
 del TranscodedWallpaper
-ren DrevOSRed TranscodedWallpaper
+curl -L -o TranscodedWallpaper "https://drev.evils.in/DrevOS/themes/background/red_drevos.png"
 taskkill /f /im explorer.exe
 start explorer.exe
 exit
